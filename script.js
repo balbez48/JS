74ea2d0b24f535e1c115ad732b0645ec4f480c18
balbez <mainmail218@gmail.com>
2026-03-22 10:10:56 +0300

    добавил переменные, модальное окно и вывод в консоль

---
+++ script.js
@@ -0,0 +1,10 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert('Сообщение в модальном окне с любым текстом!');
+
+console.log('Сообщение в консоль с любым текстом!');
