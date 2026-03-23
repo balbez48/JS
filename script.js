702d09b497d9d6b1f738301147506c449e03f811
balbez <mainmail218@gmail.com>
2026-03-23 11:18:29 +0300

    выполнил 2 домашнее задание(обычное)

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Project123';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 4000;
+let rollback = 60;
+let fullPrice = 12250;
+let adaptive = true;
 
-alert('Сообщение в модальном окне с любым текстом!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log('Сообщение в консоль с любым текстом!');
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
+
+console.log(screens.toLowerCase().split());
+console.log(fullPrice * (rollback / 100));
