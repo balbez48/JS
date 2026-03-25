1d457bb474884f18ee3c9c6e5abf33c1b1fd082e
balbez <mainmail218@gmail.com>
2026-03-25 20:05:31 +0500

    начал делать 3 простое задание

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let title = 'Project123';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 4000;
+"use strict";
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = prompt("Сколько будет стоить данная работа?");
 let rollback = 60;
 let fullPrice = 12250;
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
 console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 
 console.log(screens.toLowerCase().split());
 console.log(fullPrice * (rollback / 100));
