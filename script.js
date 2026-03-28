49559a91ca64fd1c3695520b65838fee66bc71f4
balbez <mainmail218@gmail.com>
2026-03-28 13:46:23 +0500

    доделал 3 простое домашнее задание

--- script.js
+++ script.js
@@ -2,10 +2,30 @@
 
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = prompt("Сколько будет стоить данная работа?");
+let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?"));
 let rollback = 60;
-let fullPrice = 12250;
 let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = parseInt(prompt("Сколько это будет стоить?"));
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = parseInt(prompt("Сколько это будет стоить?"));
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.round(
+  fullPrice - fullPrice * (rollback / 100),
+);
+console.log(`servicePercentPrice - ${servicePercentPrice}`);
+
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice > 0 && fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что то пошло не так");
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
